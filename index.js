58ebad0c35f3a34c483b07e944e4366d828d725e
shahar <shahar9772@gmail.com>
2023-10-01 21:32:59 +0300

    dfsdfs fsfds

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/users', userRouter);
 app.use('/tour', tourRouter);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to tour API - version 3');
+  res.send('Welcome to tour API - version 4 :-) ');
 });
 
 mongoose
